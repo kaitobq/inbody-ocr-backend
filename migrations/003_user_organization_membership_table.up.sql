58b919b08aaf6c2fa5f50d56752cf40270e86997
kaitobq <kaitobq520@gmail.com>
2024-09-19 16:30:39 +0900

    add migration file of memberships

---
+++ migrations/003_user_organization_membership_table.up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS user_organization_memberships (
+    id char(26) PRIMARY KEY,
+    user_id char(26) NOT NULL,
+    organization_id char(26) NOT NULL,
+    role ENUM('owner', 'admin', 'member') NOT NULL DEFAULT 'member',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (organization_id) REFERENCES organizations(id)
+);
