a58b62bbe0c0b12bdb3248e74512944ae326394b
kaitobq <kaitobq520@gmail.com>
2024-09-26 15:24:08 +0900

    test ci

--- internal/infra/vision_api/image_repo_impl.go
+++ internal/infra/vision_api/image_repo_impl.go
@@ -313,17 +313,17 @@ func findClosestNumberInDirection(keyword string, annotations []*visionpb.Entity
 	return closestNumber, nil
 }
 
-// // getClosestNumbersByDirection gets the closest numbers in both X and Y directions for the specified keyword.
-// func getClosestNumbersByDirection(keyword string, annotations []*visionpb.EntityAnnotation) (string, string, error) {
-// 	closestNumberX, errX := findClosestNumberInDirection(keyword, annotations, "X")
-// 	if errX != nil {
-// 		return "", "", errX
-// 	}
+// getClosestNumbersByDirection gets the closest numbers in both X and Y directions for the specified keyword.
+func getClosestNumbersByDirection(keyword string, annotations []*visionpb.EntityAnnotation) (string, string, error) {
+	closestNumberX, errX := findClosestNumberInDirection(keyword, annotations, "X")
+	if errX != nil {
+		return "", "", errX
+	}
 
-// 	closestNumberY, errY := findClosestNumberInDirection(keyword, annotations, "Y")
-// 	if errY != nil {
-// 		return "", "", errY
-// 	}
+	closestNumberY, errY := findClosestNumberInDirection(keyword, annotations, "Y")
+	if errY != nil {
+		return "", "", errY
+	}
 
-// 	return closestNumberX, closestNumberY, nil
-// }
+	return closestNumberX, closestNumberY, nil
+}
