7ad7e434819d5eb6eeed14c6a638c4200868f128
kaitobq <kaitobq520@gmail.com>
2024-09-26 14:13:42 +0900

    add log

--- internal/usecase/organization_usecase.go
+++ internal/usecase/organization_usecase.go
@@ -5,6 +5,7 @@ import (
 	"inbody-ocr-backend/internal/domain/entity"
 	"inbody-ocr-backend/internal/domain/repository"
 	"inbody-ocr-backend/internal/domain/service"
+	"inbody-ocr-backend/internal/infra/logger"
 	"inbody-ocr-backend/internal/usecase/response"
 )
 
@@ -32,16 +33,19 @@ func (uc *organizationUsecase) CreateOrganization(name string, founderID string)
 	// ユーザーが作成したい組織名と同じ名前の組織に所属していないかチェック
 	memberships, err := uc.membershipRepo.FindByUserID(founderID)
 	if err != nil {
+		logger.Error("CreateOrganization", "func", "FindByUserID()", "error", err.Error())
 		return nil, err
 	}
 	if len(*memberships) != 0 {
 		for _, membership := range *memberships {
 			org, err := uc.repo.FindByID(membership.OrganizationID)
 			if err != nil {
+				logger.Error("CreateOrganization", "func", "FindByID()", "error", err.Error())
 				return nil, err
 			}
 
 			if org.Name == name {
+				logger.Info("User tryed to create organization with same name", "name", name)
 				return nil, fmt.Errorf("cannot belong to multiple organizations with the same name [%s]", name)
 			}
 		}
@@ -49,6 +53,7 @@ func (uc *organizationUsecase) CreateOrganization(name string, founderID string)
 
 	organization, err := uc.repo.CreateOrganization(org)
 	if err != nil {
+		logger.Error("CreateOrganization", "func", "CreateOrganization()", "error", err.Error())
 		return nil, err
 	}
 
@@ -61,9 +66,11 @@ func (uc *organizationUsecase) CreateOrganization(name string, founderID string)
 	}
 	_, err = uc.membershipRepo.CreateMembership(membership)
 	if err != nil {
+		logger.Error("CreateOrganization", "func", "CreateMembership()", "error", err.Error())
 		// 作成者が組織に所属できない場合はロールバック
 		rollbackErr := uc.repo.DeleteOrganization(organization.ID)
 		if rollbackErr != nil {
+			logger.Error("CreateOrganization", "func", "DeleteOrganization()", "error", rollbackErr.Error())
 			return nil, fmt.Errorf("failed to create membership and rollback organization creation: %w", err)
 		}
 
