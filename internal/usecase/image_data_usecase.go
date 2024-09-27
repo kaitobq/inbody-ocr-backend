2e2d6a8c5c3cee07ef207c1d3e3c75708c7e363b
kaitobq <kaitobq520@gmail.com>
2024-09-27 11:27:20 +0900

    idつけ忘れ

--- internal/usecase/image_data_usecase.go
+++ internal/usecase/image_data_usecase.go
@@ -3,22 +3,26 @@ package usecase
 import (
 	"inbody-ocr-backend/internal/domain/entity"
 	"inbody-ocr-backend/internal/domain/repository"
+	"inbody-ocr-backend/internal/domain/service"
 	"inbody-ocr-backend/internal/infra/logger"
 	"inbody-ocr-backend/internal/usecase/response"
 )
 
 type imageDataUsecase struct {
 	repo repository.ImageDataRepository
+	ulidService service.ULIDService
 }
 
-func NewImageDataUsecase(repo repository.ImageDataRepository) ImageDataUsecase {
+func NewImageDataUsecase(repo repository.ImageDataRepository, ulidService service.ULIDService) ImageDataUsecase {
 	return &imageDataUsecase{
 		repo: repo,
+		ulidService: ulidService,
 	}
 }
 
 func (uc *imageDataUsecase) CreateData(weight, height, muscleWeight, fatWeight, fatPercent, bodyWater, protein, mineral float64, point uint, userID, orgID string) (*response.SaveImageDataResponse, error) {
 	imageData := &entity.ImageData{
+		ID:             uc.ulidService.GenerateULID(),
 		UserID:         userID,
 		OrganizationID: orgID,
 		Weight:         weight,
